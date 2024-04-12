94b778e2a21b1fa9af34f971fb1da13af0b9dcb6
LuisHern99 <luishernandez@wposs.com>
2024-04-12 15:09:12 -0500

    Se Crean Repositorio, Modelo, Servicio y Controladores

--- inventarios/src/main/java/gm/inventarios/InventariosApplication.java
+++ inventarios/src/main/java/gm/inventarios/InventariosApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventariosApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(InventariosApplication.class, args);
 	}
 
